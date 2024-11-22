ce5c4b22fec85bf7f3405370dad83b68070ed326
damianwysocky <damian.wysocki256@gmail.com>
2024-11-22 11:05:57 +0100

    base script

---
+++ app/app.js
@@ -0,0 +1,24 @@
+const titleInputId = 'title'
+const contentInputId = 'content'
+
+let titleInput
+let contentInput
+
+titleInput = document.getElementById(titleInputId)
+contentInput = document.getElementById(contentInputId)
+
+// - - -
+
+const createButtonId = 'create-button'
+
+let createButton
+
+createButton = document.getElementById(createButtonId)
+
+createButton.addEventListener('click', () => {
+    const title = titleInput.value
+    const content = contentInput.value
+
+    console.log(title)
+    console.log(content)
+})
